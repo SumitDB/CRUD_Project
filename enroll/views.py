6b55069d4eecddfe1205db6bf5e8f23dbdfb1c66
SumitDB <sbhatkar1994@gmail.com>
2022-07-13 02:01:56 +0530

    Days_2

--- enroll/views.py
+++ enroll/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .forms import StudentRegistration
+from .models import User
 
-# Create your views here.
+def add_show(request):
+    if request.method == "POST":
+        fm = StudentRegistration(request.POST)
+        if fm.is_valid():
+           nm = fm.cleaned_data['name']
+           email= fm.cleaned_data['email']
+           passw = fm.cleaned_data['password3']
+
+    else:
+        fm = StudentRegistration()
+    return render(request, 'enroll/addandshow.html', {"form":fm})
